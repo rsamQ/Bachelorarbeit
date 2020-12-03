0dfff15c4999fb6b34db8a988e104d7150495c61
René <sammeth-rene@web.de>
2020-12-03 16:31:25 +0100

    Implemented displaying image; Todo: fix infinity loop -> displaying Image

--- ePaper/ePaper.ino
+++ ePaper/ePaper.ino
@@ -17,6 +17,7 @@
 
 // library for fonts
 #include <Fonts/FreeMonoBold12pt7b.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
 
 // include templates
 #include "new_template.h"
@@ -33,7 +34,7 @@ GxEPD2_3C<GxEPD2_750c, GxEPD2_750c::HEIGHT / 4> display(GxEPD2_750c(/*CS=15*/ 15
 ESP8266WiFiMulti WiFiMulti;
 const char* ssid = "Smart-Fridge";
 const char* password = "BusterKeel";
-const char* imageUrl = "http://192.168.0.100:150/serverData/image/";
+const char* imageUrl = "http://192.168.0.100:150/serverData/image/image.bmp";
 const char* jsonUrl = "http://192.168.0.100:150/serverData/schedule/";
 
 //
@@ -44,6 +45,10 @@ const size_t numberOfHeaders = 2;
 String tempJson = "";
 String updateTime = "";
 
+//
+bool receiveImage = false;
+
+//
 bool connectionError = false;
 String errorMsg = "";
 
@@ -133,6 +138,7 @@ void getJsonDataFrom_HTTP(const char* url){
           // Response Header time
           updateTime = http.header("date");
 
+          //
           connectionError = false;
 
         }else {
@@ -140,7 +146,7 @@ void getJsonDataFrom_HTTP(const char* url){
           // 
           updateTime = http.header("date");
           connectionError = true;
-          errorMsg = "Error: ";
+          errorMsg = (F("Error: "));
           errorMsg += http.errorToString(httpCode).c_str();
           Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
         }
@@ -151,14 +157,88 @@ void getJsonDataFrom_HTTP(const char* url){
         
         //
         connectionError = true;
-        errorMsg = "Error: HTTP unable to connect";
+        errorMsg = (F("Error: HTTP unable to connect"));
         Serial.printf("[HTTP] Unable to connect\n");
       }     
     }
   }else{
-    
+    connectionError = true;
+    errorMsg = (F("Error: Unable to connect to WiFi"));
+    Serial.printf("Unable to connect to WiFi\n");
   }
 }
+
+/*void checkForImage(const char* url){
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+
+    WiFiClient client;
+    HTTPClient http; //must be declared after WiFiClient for correct destruction order, because used by http.begin(client,...)
+
+    Serial.print("[HTTP] begin...\n");
+
+    // configure server and url
+    http.begin(client, url);
+    //http.begin(client, "jigsaw.w3.org", 80, "/HTTP/connection.html");
+
+    Serial.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+
+        if(receiveImage == true){
+
+          SPIFFS.remove("image.bmp");
+  
+          // get lenght of document (is -1 when Server sends no Content-Length header)
+          int len = http.getSize();
+  
+          // create buffer for read
+          uint8_t buff[128] = { 0 };
+  
+          // get tcp stream
+          WiFiClient * stream = &client;
+  
+          fs::File file;
+          file = SPIFFS.open("image.bmp", "w");
+  
+          // read all data from server
+          while (http.connected() && (len > 0 || len == -1)) {
+            // read up to 128 byte
+            int c = stream->readBytes(buff, std::min((size_t)len, sizeof(buff)));
+            Serial.printf("readBytes: %d\n", c);
+            if (!c) {
+              Serial.println("read timeout");
+            }
+  
+            file.write(buff, c);
+  
+            // write it to Serial
+            Serial.write(buff, c);
+  
+            if (len > 0) {
+              len -= c;
+            }
+          }
+          file.close();
+  
+          Serial.println();
+          Serial.print("[HTTP] connection closed or file end.\n");
+        }
+        receiveImage = false;
+      }
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+  }
+}*/
 
 
 
@@ -167,21 +247,21 @@ void writeJsonTo_SPIFFS(){
   
   Serial.println(F("Saving file"));
 
-  File configFile = SPIFFS.open("test.json", "w"); //
+  File storedData = SPIFFS.open("test.json", "w"); //
   
-  if(configFile){
+  if(storedData){
     
     Serial.println(F("File opened"));
     Serial.println(F("Write file"));
     Serial.println(tempJson);
-    configFile.print(tempJson); //
+    storedData.print(tempJson); //
   }else{
     Serial.println(F("Failed to open file!"));
   }
   
   Serial.println(F("Closing file"));
   
-  configFile.close(); //
+  storedData.close(); //
 }
 
 
@@ -198,32 +278,34 @@ void drawJsonDataFromMemory(){
   int16_t heightPerDuration = 7;
   int16_t startingHour = 8;
 
-  File configFile = SPIFFS.open("test.json", "r");  //
+  File storedData = SPIFFS.open("test.json", "r");  //
 
   //
-  DynamicJsonDocument doc1(MAXIMUM_CAPACITY);
-  deserializeJson(doc1, configFile);
+  DynamicJsonDocument storedDoc(MAXIMUM_CAPACITY);
+  deserializeJson(storedDoc, storedData);
 
   //
-  JsonObject infoMode = doc1[6];
-  JsonObject room = doc1[7];
+  JsonObject infoMode = storedDoc[6];
+  JsonObject room = storedDoc[7];
+  JsonObject closedRoom = storedDoc[8];
 
   if(infoMode["mode"] == 0){
     drawTemplate();
 
     //
     display.setTextColor(GxEPD_BLACK);
-    //display.setFont();
-    display.setCursor(15, 15);
+    display.setFont(&FreeMonoBold9pt7b);
+    display.setCursor(15, 25);
     display.println(room["room"].as<char*>());  //
-    display.setCursor(15, 364);
+    display.setCursor(15, 374);
+    display.setFont();
     display.println("Last Update: " + updateTime);  // 
 
-    for(int i = 1; i <= 6; i++){
-      JsonObject repo0 = doc1[i-1].as<JsonObject>();  //
+    for(int i = 1; i <= days; i++){
+      JsonObject repo0 = storedDoc[i-1].as<JsonObject>();  //
     
-      for(int j = 1; j < 7; j++){
-
+      for(int j = 1; j <= repo0["data"].size(); j++){
+        
         int16_t hour = repo0["data"][j-1]["hour"].as<int>();  //
         int16_t minutes = repo0["data"][j-1]["minutes"].as<int>(); //
         int16_t x = offsetLeft + (i-1) * ((display.width() - offsetLeft) / days); //
@@ -240,17 +322,29 @@ void drawJsonDataFromMemory(){
         
         if(connectionError == true){
           display.setTextColor(GxEPD_RED);
-          display.setCursor(335, 364);
+          display.setCursor(335, 374);
           display.println(errorMsg);  // 
         }
       }
     }
   }else if(infoMode["mode"] == 1){
+
+    /*receiveImage = true;
+    drawBitmapFromSpiffs("image.bmp", 0, 0, false);
+    receiveImage = false;*/
     
   }else if(infoMode["mode"] == 2){
+
+    display.setTextColor(GxEPD_RED);
+    display.setFont(&FreeMonoBold12pt7b);
+    display.setCursor(display.width()/4, display.height()/4);
+    display.println(F("Dieser Raum ist momentan geschlossen!"));
+    display.setCursor((display.width()/4), (display.height()/4)+ 20);
+    display.println("Grund: ");
+    display.println(closedRoom["info"].as<char*>());
     
   }
-  configFile.close();
+  storedData.close();
 }
 
 
@@ -267,43 +361,47 @@ void compareAndDrawJsonData(){
   int16_t heightPerDuration = 7;
   int16_t startingHour = 8;
 
-  File configFile = SPIFFS.open("test.json", "r");  //
+  File storedData = SPIFFS.open("test.json", "r");  //
 
-  if(!configFile){
-    configFile.close();
+  if(!storedData){
+    storedData.close();
     writeJsonTo_SPIFFS();
-    configFile = SPIFFS.open("test.json", "r");  //
+    storedData = SPIFFS.open("test.json", "r");  //
   }
 
   //
-  DynamicJsonDocument doc1(MAXIMUM_CAPACITY);
-  deserializeJson(doc1, configFile);
+  DynamicJsonDocument storedDoc(MAXIMUM_CAPACITY);
+  deserializeJson(storedDoc, storedData);
 
   //
-  DynamicJsonDocument doc2(MAXIMUM_CAPACITY);
-  deserializeJson(doc2, tempJson);
+  DynamicJsonDocument receivedDoc(MAXIMUM_CAPACITY);
+  deserializeJson(receivedDoc, tempJson);
 
   //
-  JsonObject infoMode = doc2[6];
-  JsonObject room = doc2[7];
+  JsonObject infoMode = receivedDoc[6];
+  JsonObject room = receivedDoc[7];
+  JsonObject closedRoom = receivedDoc[8];
+
+  Serial.println(infoMode["mode"].as<int>());
 
   if(infoMode["mode"] == 0){
     drawTemplate();
 
     //
     display.setTextColor(GxEPD_BLACK);
-    //display.setFont();
-    display.setCursor(15, 15);
+    display.setFont(&FreeMonoBold9pt7b);
+    display.setCursor(15, 25);
     display.println(room["room"].as<char*>());  //
-    display.setCursor(15, 364);
+    display.setCursor(15, 374);
+    display.setFont();
     display.println("Last Update: " + updateTime);  //
     
     
-    for(int i = 1; i <= 6; i++){
-      JsonObject repo0 = doc1[i-1].as<JsonObject>();  //
-      JsonObject repo1 = doc2[i-1].as<JsonObject>();  //
+    for(int i = 1; i <= days; i++){
+      JsonObject repo0 = storedDoc[i-1].as<JsonObject>();  //
+      JsonObject repo1 = receivedDoc[i-1].as<JsonObject>();  //
     
-      for(int j = 1; j < 7; j++){
+      for(int j = 1; j <= repo1["data"].size(); j++){
 
         int16_t hour = repo1["data"][j-1]["hour"].as<int>();  //
         int16_t minutes = repo1["data"][j-1]["minutes"].as<int>(); //
@@ -334,8 +432,26 @@ void compareAndDrawJsonData(){
         }
       }
     }
+  }else if(infoMode["mode"] == 1){
+      
+    /*receiveImage = true;
+    checkForImage(imageUrl);
+    drawBitmapFromSpiffs("image.bmp", 0, 0, false);
+    receiveImage = false;
+    return;*/
+    
+  }else if(infoMode["mode"] == 2){
+    
+    display.setTextColor(GxEPD_RED);
+    display.setFont(&FreeMonoBold12pt7b);
+    display.setCursor(display.width()/4, display.height()/4);
+    display.println(F("Dieser Raum ist momentan geschlossen!"));
+    display.setCursor((display.width()/4), (display.height()/4)+ 20);
+    display.println("Grund: ");
+    display.println(closedRoom["info"].as<char*>());
+    
   }
-  configFile.close();
+  storedData.close();
 }
 
 
@@ -362,4 +478,219 @@ void loop() {
     writeJsonTo_SPIFFS();
   }
   delay(1000*60);
- };
+ }
+
+
+
+ static const uint16_t input_buffer_pixels = 800; // may affect performance
+
+static const uint16_t max_row_width = 800; // for up to 7.5" display 800x480
+static const uint16_t max_palette_pixels = 256; // for depth <= 8
+
+uint8_t input_buffer[3 * input_buffer_pixels]; // up to depth 24
+uint8_t output_row_mono_buffer[max_row_width / 8]; // buffer for at least one row of b/w bits
+uint8_t output_row_color_buffer[max_row_width / 8]; // buffer for at least one row of color bits
+uint8_t mono_palette_buffer[max_palette_pixels / 8]; // palette buffer for depth <= 8 b/w
+uint8_t color_palette_buffer[max_palette_pixels / 8]; // palette buffer for depth <= 8 c/w
+
+void drawBitmapFromSpiffs(const char *filename, int16_t x, int16_t y, bool with_color)
+{
+  fs::File file;
+  bool valid = false; // valid format to be handled
+  bool flip = true; // bitmap is stored bottom-to-top
+  uint32_t startTime = millis();
+  if ((x >= display.epd2.WIDTH) || (y >= display.epd2.HEIGHT)) return;
+  Serial.println();
+  Serial.print("Loading image '");
+  Serial.print(filename);
+  Serial.println('\'');
+  file = SPIFFS.open(filename, "r");
+  if (!file)
+  {
+    Serial.print("File not found");
+    return;
+  }
+  // Parse BMP header
+  if (read16(file) == 0x4D42) // BMP signature
+  {
+    uint32_t fileSize = read32(file);
+    uint32_t creatorBytes = read32(file);
+    uint32_t imageOffset = read32(file); // Start of image data
+    uint32_t headerSize = read32(file);
+    uint32_t width  = read32(file);
+    uint32_t height = read32(file);
+    uint16_t planes = read16(file);
+    uint16_t depth = read16(file); // bits per pixel
+    uint32_t format = read32(file);
+    if ((planes == 1) && ((format == 0) || (format == 3))) // uncompressed is handled, 565 also
+    {
+      Serial.print("File size: "); Serial.println(fileSize);
+      Serial.print("Image Offset: "); Serial.println(imageOffset);
+      Serial.print("Header size: "); Serial.println(headerSize);
+      Serial.print("Bit Depth: "); Serial.println(depth);
+      Serial.print("Image size: ");
+      Serial.print(width);
+      Serial.print('x');
+      Serial.println(height);
+      // BMP rows are padded (if needed) to 4-byte boundary
+      uint32_t rowSize = (width * depth / 8 + 3) & ~3;
+      if (depth < 8) rowSize = ((width * depth + 8 - depth) / 8 + 3) & ~3;
+      if (height < 0)
+      {
+        height = -height;
+        flip = false;
+      }
+      uint16_t w = width;
+      uint16_t h = height;
+      if ((x + w - 1) >= display.epd2.WIDTH)  w = display.epd2.WIDTH  - x;
+      if ((y + h - 1) >= display.epd2.HEIGHT) h = display.epd2.HEIGHT - y;
+      if (w <= max_row_width) // handle with direct drawing
+      {
+        valid = true;
+        uint8_t bitmask = 0xFF;
+        uint8_t bitshift = 8 - depth;
+        uint16_t red, green, blue;
+        bool whitish, colored;
+        if (depth == 1) with_color = false;
+        if (depth <= 8)
+        {
+          if (depth < 8) bitmask >>= depth;
+          //file.seek(54); //palette is always @ 54
+          file.seek(imageOffset - (4 << depth)); // 54 for regular, diff for colorsimportant
+          for (uint16_t pn = 0; pn < (1 << depth); pn++)
+          {
+            blue  = file.read();
+            green = file.read();
+            red   = file.read();
+            file.read();
+            whitish = with_color ? ((red > 0x80) && (green > 0x80) && (blue > 0x80)) : ((red + green + blue) > 3 * 0x80); // whitish
+            colored = (red > 0xF0) || ((green > 0xF0) && (blue > 0xF0)); // reddish or yellowish?
+            if (0 == pn % 8) mono_palette_buffer[pn / 8] = 0;
+            mono_palette_buffer[pn / 8] |= whitish << pn % 8;
+            if (0 == pn % 8) color_palette_buffer[pn / 8] = 0;
+            color_palette_buffer[pn / 8] |= colored << pn % 8;
+          }
+        }
+        display.clearScreen();
+        uint32_t rowPosition = flip ? imageOffset + (height - h) * rowSize : imageOffset;
+        for (uint16_t row = 0; row < h; row++, rowPosition += rowSize) // for each line
+        {
+          uint32_t in_remain = rowSize;
+          uint32_t in_idx = 0;
+          uint32_t in_bytes = 0;
+          uint8_t in_byte = 0; // for depth <= 8
+          uint8_t in_bits = 0; // for depth <= 8
+          uint8_t out_byte = 0xFF; // white (for w%8!=0 border)
+          uint8_t out_color_byte = 0xFF; // white (for w%8!=0 border)
+          uint32_t out_idx = 0;
+          file.seek(rowPosition);
+          for (uint16_t col = 0; col < w; col++) // for each pixel
+          {
+            // Time to read more pixel data?
+            if (in_idx >= in_bytes) // ok, exact match for 24bit also (size IS multiple of 3)
+            {
+              in_bytes = file.read(input_buffer, in_remain > sizeof(input_buffer) ? sizeof(input_buffer) : in_remain);
+              in_remain -= in_bytes;
+              in_idx = 0;
+            }
+            switch (depth)
+            {
+              case 24:
+                blue = input_buffer[in_idx++];
+                green = input_buffer[in_idx++];
+                red = input_buffer[in_idx++];
+                whitish = with_color ? ((red > 0x80) && (green > 0x80) && (blue > 0x80)) : ((red + green + blue) > 3 * 0x80); // whitish
+                colored = (red > 0xF0) || ((green > 0xF0) && (blue > 0xF0)); // reddish or yellowish?
+                break;
+              case 16:
+                {
+                  uint8_t lsb = input_buffer[in_idx++];
+                  uint8_t msb = input_buffer[in_idx++];
+                  if (format == 0) // 555
+                  {
+                    blue  = (lsb & 0x1F) << 3;
+                    green = ((msb & 0x03) << 6) | ((lsb & 0xE0) >> 2);
+                    red   = (msb & 0x7C) << 1;
+                  }
+                  else // 565
+                  {
+                    blue  = (lsb & 0x1F) << 3;
+                    green = ((msb & 0x07) << 5) | ((lsb & 0xE0) >> 3);
+                    red   = (msb & 0xF8);
+                  }
+                  whitish = with_color ? ((red > 0x80) && (green > 0x80) && (blue > 0x80)) : ((red + green + blue) > 3 * 0x80); // whitish
+                  colored = (red > 0xF0) || ((green > 0xF0) && (blue > 0xF0)); // reddish or yellowish?
+                }
+                break;
+              case 1:
+              case 4:
+              case 8:
+                {
+                  if (0 == in_bits)
+                  {
+                    in_byte = input_buffer[in_idx++];
+                    in_bits = 8;
+                  }
+                  uint16_t pn = (in_byte >> bitshift) & bitmask;
+                  whitish = mono_palette_buffer[pn / 8] & (0x1 << pn % 8);
+                  colored = color_palette_buffer[pn / 8] & (0x1 << pn % 8);
+                  in_byte <<= depth;
+                  in_bits -= depth;
+                }
+                break;
+            }
+            if (whitish)
+            {
+              // keep white
+            }
+            else if (colored && with_color)
+            {
+              out_color_byte &= ~(0x80 >> col % 8); // colored
+            }
+            else
+            {
+              out_byte &= ~(0x80 >> col % 8); // black
+            }
+            if ((7 == col % 8) || (col == w - 1)) // write that last byte! (for w%8!=0 border)
+            {
+              output_row_color_buffer[out_idx] = out_color_byte;
+              output_row_mono_buffer[out_idx++] = out_byte;
+              out_byte = 0xFF; // white (for w%8!=0 border)
+              out_color_byte = 0xFF; // white (for w%8!=0 border)
+            }
+          } // end pixel
+          uint16_t yrow = y + (flip ? h - row - 1 : row);
+          display.writeImage(output_row_mono_buffer, output_row_color_buffer, x, yrow, w, 1);
+        } // end line
+        Serial.print("loaded in "); Serial.print(millis() - startTime); Serial.println(" ms");
+        display.refresh();
+      }
+    }
+  }
+  file.close();
+  if (!valid)
+  {
+    Serial.println("bitmap format not handled.");
+  }
+}
+
+
+uint16_t read16(fs::File& f)
+{
+  // BMP data is stored little-endian, same as Arduino.
+  uint16_t result;
+  ((uint8_t *)&result)[0] = f.read(); // LSB
+  ((uint8_t *)&result)[1] = f.read(); // MSB
+  return result;
+}
+
+uint32_t read32(fs::File& f)
+{
+  // BMP data is stored little-endian, same as Arduino.
+  uint32_t result;
+  ((uint8_t *)&result)[0] = f.read(); // LSB
+  ((uint8_t *)&result)[1] = f.read();
+  ((uint8_t *)&result)[2] = f.read();
+  ((uint8_t *)&result)[3] = f.read(); // MSB
+  return result;
+}
